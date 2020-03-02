a1d0332c240a71fc3a71fc4356c039ca7b3da293
Jayson Chen <jayc1127@uw.edu>
2020-03-02 12:05:35 -0800

    change img path

--- src/map.js
+++ src/map.js
@@ -164,17 +164,17 @@ class Map {
         document.getElementById("percentage-text").innerHTML = `gets ${string_p}% <br>of its power from nuclear energy.`;
         var lightbulbs = Math.round(percentage);
         let all_bulbs = '';
-        for (var i = 0; i < lightbulbs; i++) all_bulbs += '<img src="/LightBulb.png" style="width: 10%;">';
-        for (var i = 0; i < 100 - lightbulbs; i++) all_bulbs += '<img src="/Dimbulb.png" style="width: 10%;">';
+        for (var i = 0; i < lightbulbs; i++) all_bulbs += '<img src="./LightBulb.png" style="width: 10%;">';
+        for (var i = 0; i < 100 - lightbulbs; i++) all_bulbs += '<img src="./Dimbulb.png" style="width: 10%;">';
         document.getElementById("lightbulbs").innerHTML += all_bulbs;
     }
 
     getFactories(working, in_progress, abandon) {
         document.getElementById("plants-container").innerHTML = '';
         let all_plants = '';
-        for (var i = 0; i < working; i++) all_plants += '<img src="/workingPlant.png" style="width: 3%;">';
-        for (var i = 0; i < in_progress; i++) all_plants += '<img src="/inProgressPlant.png" style="width: 3%;">';
-        for (var i = 0; i < abandon; i++) all_plants += '<img src="/abandon.png" style="width: 3%;">';
+        for (var i = 0; i < working; i++) all_plants += '<img src="./workingPlant.png" style="width: 3%;">';
+        for (var i = 0; i < in_progress; i++) all_plants += '<img src="./inProgressPlant.png" style="width: 3%;">';
+        for (var i = 0; i < abandon; i++) all_plants += '<img src="./abandon.png" style="width: 3%;">';
         document.getElementById("plants-container").innerHTML = all_plants;
     }
 }
