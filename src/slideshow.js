22c94fbc13a7364007d07bbaae89b4fe3e3d5de0
Jayson Chen <jayc1127@uw.edu>
2020-03-07 19:47:56 -0800

    plot plants func

--- src/slideshow.js
+++ src/slideshow.js
@@ -2,65 +2,83 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+    showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+    showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+    ChinaMapInstance.clearDots();
+    JapanMapInstance.clearDots();
+    AustraliaMapInstance.clearDots();
+    FranceMapInstance.clearDots();
+    switch (n) {
+        case 1:
+            ChinaMapInstance.plotPlants();
+            break;
+        case 2:
+            JapanMapInstance.plotPlants();
+            break;
+        case 3:
+            FranceMapInstance.plotPlants();
+            break;
+        case 4:
+            AustraliaMapInstance.plotPlants();
+            break;
+    }
+    var i;
+    var x = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
 }
 
-document.getElementById("left").addEventListener("click", function() {
+document.getElementById("left").addEventListener("click", function () {
     plusDivs(-1);
 })
 
-document.getElementById("right").addEventListener("click", function() {
+document.getElementById("right").addEventListener("click", function () {
     plusDivs(1);
 })
 
-document.getElementById("dot1").addEventListener("click", function() {
+document.getElementById("dot1").addEventListener("click", function () {
     currentDiv(1);
 })
 
-document.getElementById("dot2").addEventListener("click", function() {
+document.getElementById("dot2").addEventListener("click", function () {
     currentDiv(2);
 })
 
-document.getElementById("dot3").addEventListener("click", function() {
+document.getElementById("dot3").addEventListener("click", function () {
     currentDiv(3);
 })
 
-document.getElementById("dot4").addEventListener("click", function() {
+document.getElementById("dot4").addEventListener("click", function () {
     currentDiv(4);
 })
 
-document.getElementById("china").addEventListener("click", function() {
+document.getElementById("china").addEventListener("click", function () {
     currentDiv(1);
 })
 
-document.getElementById("japan").addEventListener("click", function() {
+document.getElementById("japan").addEventListener("click", function () {
     currentDiv(2);
 })
 
-document.getElementById("france").addEventListener("click", function() {
+document.getElementById("france").addEventListener("click", function () {
     currentDiv(3);
 })
 
-document.getElementById("australia").addEventListener("click", function() {
+document.getElementById("australia").addEventListener("click", function () {
     currentDiv(4);
 })
